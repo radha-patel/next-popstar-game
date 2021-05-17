47db12788933eff5ed9286e981614cc2894d4a76
Radha Patel <radha.rpatel02@gmail.com>
2021-05-17 17:07:55 -0400

    Add SD card set-up

---
+++ integrated_just_dance/SD_functions.ino
@@ -0,0 +1,68 @@
+void readFile(fs::FS &fs, const char * path){
+    int offset = 0;
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+      char letter = file.read();
+      Serial.print(letter);
+      sprintf(message_buffer + offset, "%c", letter);
+      offset++;
+    }
+    Serial.println(message_buffer);
+    file.close();
+}
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void double_extractor(char* data_array, double* output_values, char delimiter){
+    //your code here
+      int count = 0;
+      int help = 0;
+      int output_buffer[20];
+      char del[10] = {delimiter};
+      char * token;
+      token = strtok (data_array,del);
+      while (token != NULL)
+      {
+        help = atof(token);
+        output_values[count] = help;
+        count++;
+        token = strtok (NULL, del);
+      }
+}
